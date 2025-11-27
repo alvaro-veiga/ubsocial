25aefb493bbcb82f1758551138cefd8d3710a8a7
Álvaro Veiga <alvarojoao17@gmail.com>
2025-11-27 15:23:31 -0300

    validate apis

--- core/cursos/serializers.py
+++ core/cursos/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+from django.db.models import Avg
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
 
@@ -9,6 +10,13 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'email': {'write_only': True}
         }
+    def validate_avaliacao(self, valor):
+        if valor in range(1, 6):
+            return valor
+        raise serializers.ValidationError('A avaliação deve estar entre 1 e 5.')
+
+
+
 class CursoSerializer(serializers.ModelSerializer):
     #nested relationship
     #avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
@@ -26,6 +34,14 @@ class CursoSerializer(serializers.ModelSerializer):
         read_only=True
     )
 
+    media_avaliacoes = serializers.SerializerMethodField()
+
     class Meta:
         model = Curso
-        fields = ['id', 'titulo', 'url', 'criado_em', 'atualizado_em', 'ativo', 'avaliacoes']
+        fields = ['id', 'titulo', 'url', 'criado_em', 'atualizado_em', 'ativo', 'avaliacoes', 'media_avaliacoes']
+
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+        if media is None:
+            return 0
+        return round(media*2)/2
